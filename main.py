975e6072c495c68ccb7168a4e215b360758d0abb
MagicPojska <49831587+MagicPojska@users.noreply.github.com>
2020-10-04 20:51:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import tkinter
 import random
 
-colours = {'Red', 'Blue', 'Green', 'Black', 'Yellow', 'Orange', 'White', 'Purple', 'Brown'}
+colours = ['Red', 'Blue', 'Green', 'Pink', 'Black', 'Yellow', 'Orange', 'White', 'Purple', 'Brown']
 
 score = 0
 timeleft = 60
 
+
 def startGame(event):
     if timeleft == 60:
         countdown()
+
     nextColour()
+
 
 def nextColour():
     global score
@@ -23,15 +26,17 @@ def nextColour():
 
         e.delete(0, tkinter.END)
         random.shuffle(colours)
-        label.config( fg=str(colours[1]), text=str(colours[0]))
-        scorelabel.config(text="Score: " + str(score))
+        label.config(fg=str(colours[1]), text=str(colours[0]))
+        scoreLabel.config(text="Score: " + str(score))
+
 
 def countdown():
     global timeleft
     if timeleft > 0:
         timeleft -= 1
-        timelabel.config(text="Time left: " + str(timeleft))
-        timelabel.after(1000, countdown)
+        timeLabel.config(text="Time left: " + str(timeleft))
+        timeLabel.after(1000, countdown)
+
 
 root = tkinter.Tk()
 root.title("COLORGAME")
@@ -40,11 +45,11 @@ root.geometry("375x200")
 instructions = tkinter.Label(root, text="Type the color of the words, and not the word text!", font=('Helvetica', 12))
 instructions.pack()
 
-scorelabel = tkinter.Label(root, text="Press enter to start", font=('Helvetica', 12))
-scorelabel.pack()
+scoreLabel = tkinter.Label(root, text="Press enter to start", font=('Helvetica', 12))
+scoreLabel.pack()
 
-timelabel = tkinter.Label(root, text="Time left: " + str(timeleft), font=('Helvetica', 12))
-timelabel.pack()
+timeLabel = tkinter.Label(root, text="Time left: " + str(timeleft), font=('Helvetica', 12))
+timeLabel.pack()
 
 label = tkinter.Label(root, font=('Helvetica', 60))
 label.pack()
